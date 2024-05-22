a5c9e5ce733a916b2e328932cd5789131ded0499
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-22 11:59:00 +0200

    added style

--- App.tsx
+++ App.tsx
@@ -1,48 +1,58 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StatusBar, useColorScheme } from 'react-native';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import AllExpensesScreen from './screens/AllExpensesScreen';
 import RecentExpensesScreen from './screens/RecentExpensesScreen';
 import ManageExpensesScreen from './screens/ManageExpensesScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Entypo';
+import {GlobalStyles} from './constants/styles';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{
-      headerShown: false,
-      tabBarActiveTintColor: '#ECEA56',
-      tabBarInactiveTintColor: '#D1D0C6',
-      tabBarStyle: {
-        borderTopWidth: 0,
-        backgroundColor: '#0B3469'
-      }
-    }}
-    sceneContainerStyle= {{
-      backgroundColor: "#1552A1"
-    }}>
-      <Tab.Screen name='All' component={AllExpensesScreen} options={{
-        tabBarIcon: ({color, size}) => <Icon name="list" color={color} size={size} />
-      }}></Tab.Screen>
-      <Tab.Screen name='Recent' component={RecentExpensesScreen} options={{
-        tabBarIcon: ({color, size}) => <Icon name="star" color={color} size={size} />
-      }}></Tab.Screen>
+    <Tab.Navigator
+      screenOptions={{
+        // headerShown: false,
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+      }}
+      sceneContainerStyle={{
+        backgroundColor: GlobalStyles.colors.primary700,
+      }}>
+      <Tab.Screen
+        name="Recent"
+        component={RecentExpensesScreen}
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="calendar" color={color} size={size} />
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="All"
+        component={AllExpensesScreen}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="list" color={color} size={size} />
+          ),
+        }}></Tab.Screen>
     </Tab.Navigator>
-  )
-}
+  );
+};
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -59,21 +69,16 @@ function App(): React.JSX.Element {
       />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='Home' component={TabNavigator} options={{headerShown: false}}/>
-          <Stack.Screen name='Edit' component={ManageExpensesScreen}/>
+          <Stack.Screen
+            name="Home"
+            component={TabNavigator}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Edit" component={ManageExpensesScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff'
-  }
-});
 
 export default App;
