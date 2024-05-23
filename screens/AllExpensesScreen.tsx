89eda6106e3792f474fc9ab8d2c1987086ee72a8
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-23 15:18:49 +0200

    show correct expenses in tabs

--- screens/AllExpensesScreen.tsx
+++ screens/AllExpensesScreen.tsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
 import ExpensesOutput from "../components/ExpensesOutput";
 
 export default function AllExpensesScreen() {
+  const expenses = useSelector(
+    (state: any) => state.expenses.expenses
+  )
   return (
-    <ExpensesOutput expensesPeriod='Total' />
+    <ExpensesOutput expenses={expenses} expensesPeriod='Total' />
   )
 }
--- screens/RecentExpensesScreen.tsx
+++ screens/RecentExpensesScreen.tsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
 import ExpensesOutput from "../components/ExpensesOutput";
+import { Expense } from "../store/expenseSlice";
 
 export default function RecentExpensesScreen() {
+  const expenses = useSelector(
+    (state: any) => state.expenses.expenses
+  )
+
+  const recentExpenses = expenses.filter((expense:Expense) => {
+    const today = new Date();
+    const sevenDayAgo= new Date();
+    sevenDayAgo.setDate(today.getDate() - 7);
+    return expense.date > sevenDayAgo
+  });
   return (
-    <ExpensesOutput expensesPeriod='Last 7 days' />
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 days' />
   )
 }
