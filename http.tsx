a65690c4bbb21410ea8b1b331ecd89e145c9aead
Federica Passanante <federica.passanante@sideagroup.com>
2024-05-28 11:18:27 +0200

    implemented sign up

--- http.tsx
+++ http.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Expense } from './store/expenseSlice';
+import { FIRE_BASE_API_KEY } from '@env';
 
 const baseUrl = 'https://expense-tracker-rn-d4938-default-rtdb.firebaseio.com/';
 
@@ -38,4 +39,11 @@ export function updateExpense(id: number, expenseData: ExpenseData) {
 }
 export function deleteExpense(id: number) {
   return axios.delete(baseUrl+`/expenses/${id}.json`);
+}
+
+export async function createUser(email: string, password: string) {
+  const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+FIRE_BASE_API_KEY,
+    {email, password, returnSecureToken: true}
+  );
+  return response;
 }
--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../http';
+import LoadingOverlay from '../components/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isLoading, setIsLoading] = useState(false);
+  const onSignUp = async ({email, password}) => {
+    setIsLoading(true);
+    await createUser(email, password);
+    setIsLoading(false);
+  }
+  if (isLoading) {
+    return (
+      <>
+        <LoadingOverlay />
+        <Text>Creating user...</Text>
+      </>
+    ) 
+  }
+  return <AuthContent onAuthenticate={onSignUp} />;
 }
 
 export default SignupScreen;
